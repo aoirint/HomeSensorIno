87af451f55ba421c6911175ccfb591536ec4ce2b
aoirint <aoirint@gmail.com>
2021-07-03 06:12:19 +0900

    mhz19 co2, temperature

--- HomeSensorIno.ino
+++ HomeSensorIno.ino
@@ -4,12 +4,20 @@
 
 #include <ArduinoJson.h>
 #include <DHT.h>
+#include "MHZ19.h"
+#include <SoftwareSerial.h>
 
 #define BAUDRATE 38400
 #define PIN_DHT 3    // DHT11
 #define PIN_PHOTO A0 // フォトトランジスタ
+#define PIN_MHZ_TX 4 // Arduino Tx -- MHZ19 Rx
+#define PIN_MHZ_RX 5 // Arduino Rx -- MHZ19 Tx
+#define BAUDRATE_MHZ 9600
 
 DHT dht(PIN_DHT, DHT11);
+
+MHZ19 mhz19;
+SoftwareSerial mhz19Serial(PIN_MHZ_RX, PIN_MHZ_TX);
 
 StaticJsonDocument<128> serialJson;
 DynamicJsonDocument data(128);
@@ -19,6 +27,11 @@ void setup() {
 
   Serial.begin(BAUDRATE);
   dht.begin();
+  
+  mhz19Serial.begin(BAUDRATE_MHZ);
+  mhz19.begin(mhz19Serial);
+
+  mhz19.autoCalibration();
 }
 
 void loop() {
@@ -47,13 +60,17 @@ void writeSerial() {
   int light = analogRead(PIN_PHOTO);
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
+  
+  int mhz19Co2 = mhz19.getCO2();
+  int8_t mhz19Temperature = mhz19.getTemperature();
 
   data.clear();
   data["type"] = "sensor";
   data["light"] = light;
   data["humidity"] = humidity;
   data["temperature"] = temperature;
-  data["temperature"] = temperature;
+  data["mhz19_co2"] = mhz19Co2;
+  data["mhz19_temperature"] = mhz19Temperature;
   serializeJson(data, Serial);
   Serial.println("");
 }
